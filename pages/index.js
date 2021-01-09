499f37167d52f9dbc3d4855ad676583053ced0e3
Alexandre Ribeiro de Sá <alexribeirodesa@gmail.com>
2021-01-09 02:50:51 -0300

    teste magic key 5

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ const myMagicKey = "KEY: "+process.env.MAGIC_KEY;
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Home 5</h1>
       <p>({myMagicKey})</p>
       <p>jCMS</p>
     </div>
